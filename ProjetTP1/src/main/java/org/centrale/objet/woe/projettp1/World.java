961a6f90d59c2d84da05917e617f16444103d38a
Ferr09 <90526866+Ferr09@users.noreply.github.com>
2024-09-20 18:01:12 +0200

    Add files via upload
    Nuevo archivo de World usando HashMap (sin restricción de los limites del mundo)

--- ProjetTP1/src/main/java/org/centrale/objet/woe/projettp1/World.java
+++ ProjetTP1/src/main/java/org/centrale/objet/woe/projettp1/World.java
@@ -3,75 +3,66 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
-package org.centrale.objet.woe.projettp1;
+package org.centrale.objet.WoE.projettp;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.Random;
-import java.util.Scanner;
 
 /**
  *
- * @author fe_a8
+ * @author Fernando ROJAS
  */
 public class World {
+    HashMap<Object,Point2D> world;
     Archer robin;
     Paysan peon;
     Lapin bugs;
-    int[][] matrix;
-    int a;
-    int b;
            
     
     public World(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.printf("Indiquer la taille du monde");
-        System.out.printf("Taille horizontal : ");
-        a = scanner.nextInt();
-        System.out.printf("Taille vertical : ");
-        b = scanner.nextInt();
-        matrix = new int[a][b];    
+        robin = new Archer();
+        peon = new Paysan();
+        bugs = new Lapin();
+        world = new HashMap();
     }
     
     public void creerMondeAlea(){
         Random generateurAleatoire = new Random();
-        
-        robin = new Archer();
-        peon = new Paysan();
-        bugs = new Lapin();
-        
         //Object[] personnages = new Object[3];
         Object[] personnages = {robin, peon, bugs};
         //personnages[0] = robin;
         //personnages[1] = peon;
         //personnages[2] = bugs;
-        System.out.println(personnages[1] == peon);
         
         int i = 0;
         
         while(i < personnages.length){
-            int nbAleaX = generateurAleatoire.nextInt(a);
-            int nbAleaY = generateurAleatoire.nextInt(b);
+            int nbAleaX = generateurAleatoire.nextInt();
+            int nbAleaY = generateurAleatoire.nextInt();
+            Point2D p = new Point2D(nbAleaX,nbAleaY);
             
-            if (matrix[nbAleaX][nbAleaY] == 0){
+            if (world.containsValue(p)){
             } else {
                 Point2D point = new Point2D(nbAleaX, nbAleaY);
                 if(personnages[i].getClass() == Archer.class){
                     Archer archer = (Archer) personnages[i];
                     archer.setPos(point);
-                    matrix[nbAleaX][nbAleaY] = i;
+                    world.put(archer, point);
                     i++;
                 } else if(personnages[i].getClass() == Paysan.class){
                     Paysan paysan = (Paysan) personnages[i];
                     paysan.setPos(point);
-                    matrix[nbAleaX][nbAleaY] = i;
+                    world.put(paysan, point);
                     i++;
                 } else if(personnages[i].getClass() == Lapin.class){
                     Lapin lapin = (Lapin) personnages[i];
                     lapin.setPos(point);
-                    matrix[nbAleaX][nbAleaY] = i;
+                    world.put(lapin, point);
                     i++;
                 } else if(personnages[i].getClass() == Monstre.class){
                     Monstre monstre = (Monstre) personnages[i];
                     monstre.setPos(point);
-                    matrix[nbAleaX][nbAleaY] = i;
+                    world.put(monstre, point);
                     i++;
                 } else {
                     System.out.println("Personnage pas identifié");
