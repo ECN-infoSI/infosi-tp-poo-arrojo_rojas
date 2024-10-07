f93e5a904f3fc0bbccb08575619fe3d0bc5f669e
augustoarrojo <augustoarrojo@mbp-de-augusto.home>
2024-10-07 16:59:26 +0200

    Joueur e Deplacable

--- ProjetTP1/src/main/java/org/centrale/objet/woe/projettp1/World.java
+++ ProjetTP1/src/main/java/org/centrale/objet/woe/projettp1/World.java
@@ -177,4 +177,9 @@ public class World {
         }
         return true;       
     }
+    
+    public Personnage CreationJoueur () {
+        /*Personnage Joueur = new Joueur();
+        return Joueur;*/
+    };
 }
